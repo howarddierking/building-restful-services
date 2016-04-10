8a8bddbfd26f325d6ea3eecf736ef4b6dc9aa815
Howard Dierking <howarddierking@gmail.com>
2016-04-10 15:13:17 -0700

    working index.html page with promise cleanup; added bug 789

--- module2/hal-json/client/main.js
+++ module2/hal-json/client/main.js
@@ -1,60 +1,73 @@
-'use strict';
-
-let getResource = function(url) {
-  let resource = new Hyperagent.Resource(url);
-  return resource.fetch();
-};
-
-let getPipelines = function(url){
-  let resource = new Hyperagent.Resource(url);
-  return getResource(url).then(function(root) {
-    return _.pick(root.links, 'backlog', 'inProgress');
-  });
-};
+let websiteRoot = 'http://m2.build-rest.net/hal-json/';
 
 let capitalize = function (string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-
-let getBugInfo = function(array) {
-  let bugUrlArray = [];
-  array.forEach(function(bugUrl){
-    let bug = new Hyperagent.Resource(bugUrl.props.href);
-    bugUrlArray.push(bug.fetch());
-  });
-  return bugUrlArray;
-};
-
-let getWatchedByOrAssignedTo = function(array) {
-  return Promise.all(array.map(function(userUrl){
-    let api = new Hyperagent.Resource(userUrl.props.href);
-    return api.fetch().then(function(userArray) {
-      return userArray;
+let renderPipeline = function(bugs, name){
+  return Q.fcall(function(){
+    return '<div class="col-md-3 bug-column"><h2><small>'
+      .concat(name)
+      .concat('</small></h2><ul class="bugs-ul list-group">')
+  })
+  .then(function(m){
+    return Q.all(bugs.map(renderBug)).then(function(vals){
+      return m.concat(vals.join(''));
     });
-  }));
-};
-
-let buildBugDash = function(promiseArray) {
-  promiseArray.forEach(function(bugPromise, index){
-    Promise.resolve(bugPromise).then(function(bug){
-      $('.bugs-ul').append('<li class="list-group-item"></li>');
-      let bugItem = $('.list-group-item:nth-child(' + (index + 1) + ')');
-      bugItem.append('<h4><a class="bug-title">' + capitalize(bug.props.title) + '</a></h4>')
-      .append('<h5>' + capitalize(bug.props.description) + '</h5>');
-
-      Promise.resolve(getWatchedByOrAssignedTo(bug.links.assignedTo)).then(function(userArray){
-        userArray.forEach(function(user) {
-          bugItem.append('<p>Assigned to: <a class="assigned-to">' + capitalize(user.props.username) + '</a></p>');
-        });
-      });
-
-      Promise.resolve(getWatchedByOrAssignedTo(bug.links.watchedBy)).then(function(userArray){
-        userArray.forEach(function(user) {
-          bugItem.append('<p>Watched by: <a class="watched-by">' + capitalize(user.props.username) + '</a></p>');
-        });
-      });
-    });
+  })
+  .then(function(m){
+    return m.concat('</ul></div>');
   });
 };
 
+let renderBug = function(bugLink){
+  return bugLink.fetch()
+  .then(function(bugResource){
+    
+    return Q.fcall(function(){
+      return '<li class="list-group-item">'
+        .concat('<h4><a class="bug-title" href="')
+        .concat(websiteRoot + '/bug.html#' + encodeURI(bugLink.href))
+        .concat('">')
+        .concat(capitalize(bugResource.props.title))
+        .concat('</a></h4><h5>')
+        .concat(capitalize(bugResource.props.description))
+        .concat('</h5>');
+    })
+    .then(function(m){
+      return Q.all(bugResource.links.assignedTo.map(renderAssignee)).then(function(vals){
+        return m.concat(vals.join(''));
+      });
+    })
+    .then(function(m){
+      return Q.all(bugResource.links.watchedBy.map(renderWatcher)).then(function(vals){
+        return m.concat(vals.join(''));
+      });
+    })
+    .then(function(m){
+      return m.concat('</li>');
+    });
+  });
+};
+
+let renderAssignee = function(assigneeLink){
+  return assigneeLink.fetch()
+  .then(function(userResource){
+    return '<p>Assigned to: <a href="'
+      .concat(websiteRoot + 'user.html#' + encodeURI(assigneeLink.href))
+      .concat('" class="assigned-to">')
+      .concat(capitalize(userResource.props.username))
+      .concat('</a></p>');
+  });
+};
+
+let renderWatcher = function(watcherLink){
+  return watcherLink.fetch()
+  .then(function(userResource){
+    return '<p>Watched by: <a class="watched-by" href="'
+      .concat(websiteRoot + 'user.html#' + encodeURI(watcherLink.href))
+      .concat('">')
+      .concat(capitalize(userResource.props.username))
+      .concat('</a></p>');
+  });
+};
