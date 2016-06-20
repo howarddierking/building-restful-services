6c3cdd09c708effbb79f2eb9999390c8af6deab3
Howard Dierking <howarddierking@gmail.com>
2016-06-20 08:20:03 -0700

    module 5 skeleton

---
+++ module5/client-json/render.js
@@ -0,0 +1,41 @@
+let websiteRoot = '/client-json';
+
+let getResource = function(url, callback){
+  $.getJSON(url, callback)
+};
+
+let getPipelines = function(url, callback){
+  getResource(url, function(data) {
+    callback(_.pick(data, 'backlog', 'inProgress'));
+  });
+};
+
+let capitalize = function (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+let renderBugUser = function(roleStatement, user){
+  return `<p>${roleStatement}: 
+    <a href="${websiteRoot}/user.html#${encodeURI(user.self)}" class="${roleStatement.toLowerCase().replace(' ', '-')}">
+      ${capitalize(user.username)}
+    </a></p>`;
+};
+
+let renderBug = function(bug){
+  return `<li class="list-group-item">
+    <h4><a class="bug-title" href="${websiteRoot}/bug.html#${encodeURI(bug.self)}">
+      ${capitalize(bug.title)}
+    </a></h4>
+    <h5>${capitalize(bug.description)}</h5>
+    ${_.map(bug.assignedTo, _.partial(renderBugUser, 'Assigned to')).join('')}
+    ${_.map(bug.watchedBy, _.partial(renderBugUser, 'Watched by')).join('')}
+    </li>`;
+};
+
+let renderPipeline = function(pipeline, pipelineName){
+  return `<div class="col-md-3 bug-column">
+    <h2><small>${pipelineName}</small></h2>
+    <ul class="bugs-ul list-group">
+      ${_.map(pipeline, renderBug).join('')}
+    </ul></div>`;
+};
