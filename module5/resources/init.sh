f9c2f1b18f80c5cef0408bf8db78948e1ad2ca1b
Howard Dierking <howard.dierking@concur.com>
2016-06-23 17:33:53 -0700

    split module 5 into 5a and 5b (where 5a still uses nginx and focuses on the client implications)
    updated nginx configuration to enable all origins
    updated nginx configuration to fake out post requests
    
    (verified in editor.swagger.io)

--- module5/resources/init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-/resources/wait-for-it.sh db:28015 -- rethinkdb-import -f /resources/bugs.json --table restbugs.bugs -c db:28015
-/resources/wait-for-it.sh db:28015 -- rethinkdb-import -f /resources/users.json --table restbugs.users -c db:28015
